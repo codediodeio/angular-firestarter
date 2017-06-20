76854c528910ef87f6a46fe524740dc21b586586
codediodeio <hello@codediode.io>
2017-06-20 07:18:05 -0700

    fix: router guard potential mem leak

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,22 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { AuthService } from './auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private afAuth: AngularFireAuth, private router: Router) {}
 
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | boolean {
-      if (this.auth.authenticated) { return true; }
 
-      return this.auth.currentUserObservable
+      return this.afAuth.authState
            .take(1)
            .map(user => !!user)
            .do(loggedIn => {
