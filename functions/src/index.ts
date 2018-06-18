df57e9175caec647c990bca9d3af4ad158ab677a
codediodeio <hello@codediode.io>
2018-06-18 07:33:48 -0700

    add badges to ui

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,6 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const holaMundo = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
