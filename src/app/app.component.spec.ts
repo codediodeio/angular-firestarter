565f1f5e6c4fa5fd17ca02238379cd62139e53d5
codediodeio <hello@codediode.io>
2018-06-18 07:04:45 -0700

    fix: broken spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -36,6 +36,6 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('foo');
+    expect(app.title).toEqual('app');
   }));
 });
