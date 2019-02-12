315a3645722e5f60757146f1cd7b52c5fa182bcd
Nino Ben Payoyo <nbcpayoyo@gmail.com>
2019-02-12 11:44:33 +0800

    fix for new user auth not created

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,11 +7,19 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 export const createFirebaseToken = functions.https.onCall(async(data, context) => {
   const uid = data.uid;
-  const user = await admin.auth().getUser(uid);
 
-  if (!user) {
-    console.log(`Creating user: ${JSON.stringify(data)}`)
-    await admin.auth().createUser(data);
+  try {
+    await admin.auth().getUser(uid);
+  }
+  catch (err) {
+    // Create New User Auth
+    if (err.code === 'auth/user-not-found') {
+      console.log(`Creating user: ${JSON.stringify(data)}`);
+      await admin.auth().createUser(data);
+    } else {
+      console.log(`Unknown Problem: ${JSON.stringify(err)}`);
+      return err;
+    }
   }
 
   return admin.auth().createCustomToken(uid, data).then((token) => {
