e31d1f002ecc0a9e66df4b3458ccb1ba252ec60b
unknown <hello@jeffdelaney.me>
2019-09-05 10:10:45 -0700

    rm old

--- cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-// const wp = require('@cypress/webpack-preprocessor')
-
-module.exports = (on) => {
-    // Uncomment for TS support
-    // const options = {
-    //     webpackOptions: require('../webpack.config'),
-    // }
-    // on('file:preprocessor', wp(options))
-}
