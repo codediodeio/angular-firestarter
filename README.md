3b4c8a83a617e9674947adc86b86ce9e166594a2
unknown <hello@jeffdelaney.me>
2020-02-12 20:41:50 -0700

    upgrade: angular 9

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Firestarter is an Angular PWA powered by Firebase.
 
 ## Features
 
-- Angular 8.x + Firebase
+- Angular 9.x + Firebase
 - Installable PWA
 - OAuth and Email/Password Signup with Firebase
 - Drag & drop Kanban demo with Firestore
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
