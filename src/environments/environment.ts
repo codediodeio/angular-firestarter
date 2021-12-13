dd922378ba4de62ce28c8253622c8e4fa64cdd04
Ismaël Meziani <meziani-i@outlook.fr>
2021-12-13 22:29:37 +0100

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase:  {
-    apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    authDomain: 'angular-voxer.firebaseapp.com',
-    databaseURL: 'https://angular-voxer.firebaseio.com',
-    projectId: 'angular-voxer',
-    storageBucket: 'angular-voxer.appspot.com',
-    messagingSenderId: '122050526979',
-    appId: '1:122050526979:web:529304dce5ffca7a'
+    apiKey: "AIzaSyA7lpOtvRwOmIhA2-YDiv_hOZx_w62qFyw",
+    authDomain: "beerus-crud.firebaseapp.com",
+    databaseURL: "https://beerus-crud-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "beerus-crud",
+    storageBucket: "beerus-crud.appspot.com",
+    messagingSenderId: "843677755888",
+    appId: "1:843677755888:web:250cb241346e7a1d8b9535",
+    measurementId: "G-N957X3XX0R"
   }
 };
 
